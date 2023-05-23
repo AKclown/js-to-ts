8df8964d4186940f3236d4c7fa627ae37dcd72b2
AKclown <13413629530@139.com>
2023-05-23 10:14:12 +0800

    refactor: Use traverse to traverse AST data and generate a new type syntax tree

--- src/Main.ts
+++ src/Main.ts
@@ -1,12 +1,13 @@
 import { window } from "vscode";
+import * as t from "@babel/types";
+import generate from "@babel/generator";
+import traverse, { NodePath, Visitor } from "@babel/traverse";
+import { ParseResult, parse, parseExpression } from "@babel/parser";
+import { v4 as uuidv4 } from "uuid";
+import { isArray, isObj } from "./utils";
+import { attributeSort } from "./helper";
 import { BaseClass } from "./BaseClass";
 import { IMain } from "./interface/Main.interface";
-import generate from "@babel/generator";
-import * as t from "@babel/types";
-import { parse, parseExpression } from "@babel/parser";
-import { v4 as uuidv4 } from "uuid";
-import { isArray, isObj } from './utils';
-import { strParse, attributeSort } from './helper';
 
 export enum AstTypeEnum {
   identifier = "Identifier",
@@ -14,8 +15,8 @@ export enum AstTypeEnum {
   numericLiteral = "NumericLiteral",
   booleanLiteral = "BooleanLiteral",
   nullLiteral = "NullLiteral",
-  templateLiteral = 'TemplateLiteral',
-  bigintLiteral = 'BigintLiteral',
+  templateLiteral = "TemplateLiteral",
+  bigintLiteral = "BigintLiteral",
   newExpression = "NewExpression",
   callExpression = "CallExpression",
   objectExpression = "ObjectExpression",
@@ -118,7 +119,6 @@ ${interfaceText.trim()}${content}
     return contentText;
   }
 
-
   // *********************
   // API TO TS
   // *********************
@@ -153,7 +153,9 @@ ${interfaceText.trim()}${content}
       if (isObj(value)) {
         return this.isChild(attributeSort(value), key, types);
       } else if (isArray(value)) {
-        return value.every((item: any) => this.isChild(attributeSort(item), key, types));
+        return value.every((item: any) =>
+          this.isChild(attributeSort(item), key, types)
+        );
       }
       return false;
     });
@@ -164,18 +166,19 @@ ${interfaceText.trim()}${content}
    */
   getObjectByStr(str: string, level: number = 0): any {
     const entries = [];
-    while (str.indexOf('{') !== -1) {
+    while (str.indexOf("{") !== -1) {
       let stackLen = 0;
-      let left = str.indexOf('{'), right = left;
+      let left = str.indexOf("{"),
+        right = left;
       if (left !== -1) {
         stackLen++;
       }
 
       while (stackLen) {
         right++;
-        if (str[right] === '}') {
+        if (str[right] === "}") {
           stackLen--;
-        } else if (str[right] === '{') {
+        } else if (str[right] === "{") {
           stackLen++;
         }
       }
@@ -187,7 +190,7 @@ ${interfaceText.trim()}${content}
       }
 
       right = left + 1;
-      while (str[left] !== ';' && left >= 0) {
+      while (str[left] !== ";" && left >= 0) {
         left--;
       }
       const key = str.slice(left + 1, right);
@@ -196,7 +199,12 @@ ${interfaceText.trim()}${content}
       entries.push([key, value]);
     }
 
-    entries.push(...(str.split(';').map(item => item.split('?:'))).filter(item => item[0] && item[1]));
+    entries.push(
+      ...str
+        .split(";")
+        .map((item) => item.split("?:"))
+        .filter((item) => item[0] && item[1])
+    );
     return Object.fromEntries(entries); // 返回一个对象
   }
 
@@ -204,10 +212,14 @@ ${interfaceText.trim()}${content}
    * 移除接口类型名称以及注释
    */
   getInterfaceInnerText(code: string): string {
-    let left = code.indexOf('{') + 1;
+    let left = code.indexOf("{") + 1;
     // 这里需要先去除单行注释，再去除空格，最后去除多行注释
     // 暂时未排除为何不能使用/(\/\/.*\n|\/\*(.|\n)*\*\/)/一次性去除注释，该正则浏览器可正确执行
-    return code.slice(left, code.length - 1).replace(/\/\/.*\n/g, '').replace(/(\n|\s)/g, '').replace(/\/\*(.|\n)*\*\//, '');
+    return code
+      .slice(left, code.length - 1)
+      .replace(/\/\/.*\n/g, "")
+      .replace(/(\n|\s)/g, "")
+      .replace(/\/\*(.|\n)*\*\//, "");
   }
 
   /**
@@ -224,9 +236,9 @@ ${interfaceText.trim()}${content}
       const stack = [code[right]];
       while (stack.length) {
         right++;
-        if (['(', '[', '{'].includes(code[right])) {
+        if (["(", "[", "{"].includes(code[right])) {
           stack.push(code[right]);
-        } else if ([')', ']', '}'].includes(code[right])) {
+        } else if ([")", "]", "}"].includes(code[right])) {
           stack.pop();
         }
       }
@@ -234,7 +246,6 @@ ${interfaceText.trim()}${content}
     });
     return code;
   }
-
 
   apiToTs(code: string) {
     const expressionAst: any = parseExpression(code);
@@ -271,16 +282,23 @@ ${interfaceText.trim()}${content}
 
       if (regualar.test(text)) {
         // TODO: 类型未定义
-        const ast: any = parse(text, { plugins: ["typescript"] });
-        const declaration = ast.program.body[0].declarations[0];
-        variableName = declaration.id.name;
-        type = declaration.init.type;
+        const ast: ParseResult<t.File> = parse(text, {
+          plugins: ["typescript"],
+        });
+        this.traverseCode(ast);
 
-        if (type === AstTypeEnum.objectExpression) {
-          data = declaration.init.properties;
-        } else if (type === AstTypeEnum.arrayExpression) {
-          data = declaration.init.elements;
-        }
+        const code = generate(ast).code;
+        console.log("code: ", code);
+
+        // const declaration = ast.program.body[0].declarations[0];
+        // variableName = declaration.id.name;
+        // type = declaration.init.type;
+
+        // if (type === AstTypeEnum.objectExpression) {
+        //   data = declaration.init.properties;
+        // } else if (type === AstTypeEnum.arrayExpression) {
+        //   data = declaration.init.elements;
+        // }
       } else {
         // 不能存在分号
         const updateText = text.trimRight().replace(/;$/, "");
@@ -293,9 +311,10 @@ ${interfaceText.trim()}${content}
           data = expressionAst.elements;
         }
       }
-      const code = this.analyzeAndGenerate(data, type, variableName);
-      const replaceText = this.selfCallingName(code, `I${variableName}`); // 启用自调用
-      editor.edit((editorContext) => editorContext.replace(range, replaceText));
+
+      // const code = this.analyzeAndGenerate(data, type, variableName);
+      // const replaceText = this.selfCallingName(code, `I${variableName}`); // 启用自调用
+      // editor.edit((editorContext) => editorContext.replace(range, replaceText));
     });
   }
 
@@ -401,5 +420,256 @@ ${interfaceText.trim()}${content}
     }
     // 未知类型
     return t.tsUnknownKeyword();
+  }
+
+  // *********************
+  // Traverse
+  // *********************
+
+  traverseCode(ast: ParseResult<t.File>) {
+    traverse(ast, this.ObjectVisitor);
+    console.log("ast: ", ast);
+    traverse(ast, this.VariableVisitor);
+  }
+
+  // *********************
+  // Visitor
+  // *********************
+
+  /** 对象Visitor */
+  get ObjectVisitor(): Visitor<t.Node> {
+    const _that = this;
+    return {
+      ObjectProperty(path: NodePath<t.ObjectProperty>, state: any = {}) {
+        const key = (path.node.key as t.Identifier).name;
+        const value = path.node.value;
+        let typeAnnotation: t.TSType = t.tsUnknownKeyword();
+        if (t.isStringLiteral(value) || t.isTemplateLiteral(value)) {
+          typeAnnotation = t.tsStringKeyword();
+        } else if (
+          t.isBigIntLiteral(value) ||
+          t.isDecimalLiteral(value) ||
+          t.isNumericLiteral(value)
+        ) {
+          typeAnnotation = t.tsNumberKeyword();
+        } else if (t.isBooleanLiteral(value)) {
+          typeAnnotation = t.tsBooleanKeyword();
+        } else if (t.isNullLiteral(value)) {
+          typeAnnotation = t.tsNullKeyword();
+        } else if (t.isRegExpLiteral(value)) {
+          typeAnnotation = t.tsTypeReference(t.identifier("RegExp"));
+        } else if (t.isIdentifier(value)) {
+          if (value.name === "undefined") {
+            typeAnnotation = t.tsUndefinedKeyword();
+          } else if (value.name === "Symbol") {
+            typeAnnotation = t.tsSymbolKeyword();
+          }
+        } else if (t.isNewExpression(value)) {
+          const calleeName = generate((value as t.NewExpression).callee).code;
+          if (calleeName === "Promise") {
+            typeAnnotation = t.tsTypeReference(
+              t.identifier(`${calleeName}<unknown>`)
+            );
+          }
+          typeAnnotation = t.tsTypeReference(t.identifier(calleeName));
+        } else if (t.isCallExpression(value)) {
+          const calleeName = generate((value as t.CallExpression).callee).code;
+          typeAnnotation = t.tsTypeReference(t.identifier(calleeName));
+        } else if (t.isArrowFunctionExpression(value)) {
+          // TODO: 未完成
+          // typeAnnotation = t.tsFunctionType(null, []);
+        } else if (t.isArrayExpression(value)) {
+          if (value.elements.length) {
+            const id = uuidv4().slice(0, 4);
+            state.levelRecord = [];
+            path.get("value").traverse(_that.ArrayVisitor, state);
+            // 生成一个新的变量
+            const variable = t.variableDeclaration("const", [
+              t.variableDeclarator(
+                t.identifier(`I${id}`),
+                path.node.value as t.ArrayExpression
+              ),
+            ]);
+            const programNode = path.findParent((path) => path.isProgram())!;
+            (programNode.node as t.Program).body.push(variable);
+            typeAnnotation = t.tsTypeReference(t.identifier(`Array<I${id}>`));
+          } else {
+            typeAnnotation = t.tsTypeReference(t.identifier(`Array<unknown>`));
+          }
+          path.skip();
+        } else if (t.isObjectExpression(value)) {
+    
+
+          if (_that.isALLKeySame(path)) {
+            console.log('path: ', path);
+            console.log("parentObject: ", path.node);
+            console.log("parentObject: ", path.parent);
+          } else {
+            const id = uuidv4().slice(0, 4);
+            path.get("value").traverse(_that.ObjectVisitor, state);
+            // 生成一个新的变量
+            const variable = t.variableDeclaration("const", [
+              t.variableDeclarator(
+                t.identifier(`I${id}`),
+                path.node.value as t.ObjectExpression
+              ),
+            ]);
+            const programNode = path.findParent((path) => path.isProgram())!;
+            (programNode.node as t.Program).body.push(variable);
+            typeAnnotation = t.tsTypeReference(t.identifier(`I${id}`));
+          }
+
+          path.skip();
+        }
+        const node = t.tsPropertySignature(
+          t.identifier(key),
+          t.tsTypeAnnotation(typeAnnotation)
+        );
+        path.replaceWith(node);
+      },
+    };
+  }
+
+  /** 数组处理 */
+  get ArrayVisitor(): any {
+    const _that = this;
+    return {
+      ObjectExpression(path: NodePath<t.ObjectExpression>, state: any) {
+        console.log("path.parent: 2", path.parent);
+
+        path.traverse(_that.ObjectVisitor, state);
+        path.replaceWith(
+          t.tsTypeLiteral(path.node.properties as unknown as t.TSTypeElement[])
+        );
+        path.skip();
+      },
+      ArrayExpression(path: NodePath<t.ArrayExpression>, state: any) {
+        state.levelRecord = [];
+        if (path.node.elements.length) {
+          path.traverse(_that.ArrayVisitor, state);
+          path.replaceWith(
+            t.tsArrayType(
+              t.tsUnionType(path.node.elements as unknown as t.TSType[])
+            )
+          );
+        } else {
+          path.replaceWith(t.tsArrayType(t.tsUnknownKeyword()));
+        }
+        path.skip();
+      },
+      "StringLiteral|TemplateLiteral"(path: NodePath<t.Literal>, state: any) {
+        if (state.levelRecord.includes("string")) {
+          path.remove();
+        } else {
+          path.replaceWith(t.tsStringKeyword());
+          state.levelRecord.push("string");
+        }
+      },
+      "NumericLiteral|BigIntLiteral|DecimalLiteral"(
+        path: NodePath<t.Literal>,
+        state: any
+      ) {
+        if (state.levelRecord.includes("number")) {
+          path.remove();
+        } else {
+          path.replaceWith(t.tsNumberKeyword());
+          state.levelRecord.push("number");
+        }
+      },
+      BooleanLiteral(path: NodePath<t.BooleanLiteral>, state: any) {
+        if (state.levelRecord.includes("boolean")) {
+          path.remove();
+        } else {
+          path.replaceWith(t.tsBooleanKeyword());
+          state.levelRecord.push("boolean");
+        }
+      },
+      NullLiteral(path: NodePath<t.NullLiteral>, state: any) {
+        if (state.levelRecord.includes("null")) {
+          path.remove();
+        } else {
+          path.replaceWith(t.tsNullKeyword());
+          state.levelRecord.push("null");
+        }
+      },
+      Identifier(path: NodePath<t.Identifier>, state: any) {
+        if (
+          path.node.name === "undefined" &&
+          !state.levelRecord.includes("undefined")
+        ) {
+          path.replaceWith(t.tsUndefinedKeyword());
+          state.levelRecord.push("undefined");
+        } else if (!state.levelRecord.includes("unknown")) {
+          path.replaceWith(t.tsUnknownKeyword());
+          state.levelRecord.push("unknown");
+        }
+        path.remove();
+      },
+    };
+  }
+
+  /** 变量处理 */
+  get VariableVisitor(): Visitor<t.Node> {
+    return {
+      VariableDeclaration(path: NodePath<t.VariableDeclaration>) {
+        let declarations = path.node.declarations
+          .map((declaration) => {
+            const name = (declaration.id as t.Identifier).name;
+            if (t.isObjectExpression(declaration.init)) {
+              return t.tsInterfaceDeclaration(
+                t.identifier(`I${name}`),
+                null,
+                null,
+                t.tsInterfaceBody(
+                  (declaration.init as t.ObjectExpression)
+                    .properties as unknown as Array<t.TSTypeElement>
+                )
+              );
+            } else if (t.isArrayExpression(declaration.init)) {
+              return t.tsTypeAliasDeclaration(
+                t.identifier(`${name}`),
+                null,
+                t.tSUnionType(
+                  (declaration.init as t.ArrayExpression)
+                    .elements as unknown as Array<t.TSType>
+                )
+              );
+            }
+          })
+          .filter(Boolean) as Array<
+          t.TSInterfaceDeclaration | t.TSTypeAliasDeclaration
+        >;
+
+        path.replaceWithMultiple(declarations);
+      },
+    };
+  }
+
+  // *********************
+  // Utils Funtion
+  // *********************
+  /** 父子key是否一致 */
+  isALLKeySame(path: NodePath<t.ObjectProperty>): boolean {
+    // 找到最近一个父节点为"ObjectExpression"的Node
+    const parentObject = path.findParent((path) => path.isObjectExpression());
+    const parentProps =
+      (parentObject?.node as t.ObjectExpression).properties ?? [];
+    const childProps = (path.node.value as t.ObjectExpression).properties ?? [];
+    // 获取到所有的key
+    const parentKeys = parentProps.map(
+      (props) => ((props as t.ObjectProperty).key as t.Identifier).name
+    );
+    const childKeys = childProps.map(
+      (props) => ((props as t.ObjectProperty).key as t.Identifier).name
+    );
+
+    if (
+      childKeys.length &&
+      parentKeys.length === childKeys.length &&
+      childKeys.every((key) => parentKeys.includes(key))
+    ) {
+      return true;
+    }
+    return false;
   }
 }
